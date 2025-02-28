d4cbee708d1ec8bb4110c947d8ad3095a66cf481
AmaanNaseh <naseh.amaan@gmail.com>
2025-02-28 23:08:38 +0530

    updated backend

--- kisan-e-dukaan/backend/server.js
+++ kisan-e-dukaan/backend/server.js
@@ -10,11 +10,14 @@ import couponRoutes from "./routes/coupon.route.js";
 
 import analyticsRoutes from "./routes/analytics.route.js";
 
+import cors from "cors";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
