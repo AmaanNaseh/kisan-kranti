97bea96149bab4eeea64c5e2e32ab1fd75762cd7
AmaanNaseh <naseh.amaan@gmail.com>
2025-10-14 23:20:07 +0530

    updated config.js

--- kisan-kranti/frontend/src/Config/Config.js
+++ kisan-kranti/frontend/src/Config/Config.js
@@ -1,9 +1,9 @@
 // ----------------- CROP DISEASES -----------------
 
-// export const Flask_Backend_API = "/not-deployed";
-export const Flask_Backend_API = "http://localhost:8000";
+export const Flask_Backend_API = "/not-deployed";
+// export const Flask_Backend_API = "http://localhost:8000";
 
 // ----------------- EDukaan -----------------
 
-export const E_Dukaan_Backend_API = "http://localhost:5000";
-// export const E_Dukaan_Backend_API = "https://e-dukaan-backend.onrender.com";
+// export const E_Dukaan_Backend_API = "http://localhost:5000";
+export const E_Dukaan_Backend_API = "https://e-dukaan-backend.onrender.com";
